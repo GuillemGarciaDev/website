ae776daf642166c89df8cdca99f40cb9cbfb823d
Guillem García <guillem.garcia.gil@protonmail.com>
2021-11-25 19:56:24 +0100

    ✅ Bogart Font uploaded and added to theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Text } from '@chakra-ui/react'
 import theme from '../utils/theme'
+import Fonts from '../components/Fonts'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
+			<Fonts />
 			<Component {...pageProps} />
 		</ChakraProvider>
 	)
 }
 
 export default MyApp
+
