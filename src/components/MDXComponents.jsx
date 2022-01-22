2efcea89097b957d03091298b232e5322f07c17a
Guillem García <guillem.garcia.gil@protonmail.com>
2022-01-22 18:28:35 +0100

    Img added to MDXComponents

--- src/components/MDXComponents.jsx
+++ src/components/MDXComponents.jsx
@@ -1,7 +1,8 @@
-import {Heading, Text} from '@chakra-ui/react'
+import {Heading, Text, Image} from '@chakra-ui/react'
 
 export default {
     h1: (props) => <Heading as='h1' {...props}/>,
     h2: (props) => <Heading as='h2' {...props}/>,
     p:  (props) => <Text fontFamily='Montserrat' {...props}/>,
+    img: (props) => <Image width='100%' height='100%' />
 }
