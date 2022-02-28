447d1e91694b8094074634cce2ada1e08509f0bc
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-02-28 17:20:24 +0100

    Quick fix

--- src/components/MDXComponents.jsx
+++ src/components/MDXComponents.jsx
@@ -1,8 +1,10 @@
-import {Heading, Text, Image} from '@chakra-ui/react'
+import {Heading, Text, Image, Flex} from '@chakra-ui/react'
 
 export default {
-    h1: (props) => <Heading as='h1' {...props}/>,
-    h2: (props) => <Heading as='h2' {...props}/>,
-    p:  (props) => <Text fontFamily='Montserrat' {...props}/>,
-    img: (props) => <Image width='100%' height='100%' />
+    h1: (props) => <Heading margin='1rem 1rem 2rem' as='h1' size='2xl' {...props}/>,
+    h2: (props) => <Heading margin='1rem 1rem' as='h2' size='xl' {...props} />,
+    h3: (props) => <Heading margin='1rem 1rem' as='h3' size='lg' {...props}/>,
+    p:  (props) => <Text margin='1rem 1rem' fontSize='16px' fontFamily='Montserrat' {...props}/>,
+    img: (props) => <Flex flexDirection='row' justifyContent='center' alignItems='center'><Image alignSelf='center' margin='2rem 1rem' width='80%' height='80%' {...props}/></Flex>
+    
 }
